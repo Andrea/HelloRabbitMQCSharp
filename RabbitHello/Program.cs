2c6fac3d4f5172645ee7c1df38090343f4c10cc2
unknown <roundcrisis@gmail.com>
2011-08-31 18:46:05 +0100

    first commit

---
+++ RabbitHello/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabbitHello
+{
+	using RabbitMQ.Client;
+	using RabbitMQ.Client.Framing.v0_8;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var connectionFactory = new ConnectionFactory();
+			connectionFactory.HostName = "localhost";
+			using (var connection = connectionFactory.CreateConnection())
+			{
+				using (IModel model = connection.CreateModel())
+				{
+					model.QueueDeclare("BLAH", false, false, false, new Dictionary<string, object>());
+					var properties = model.CreateBasicProperties();
+					var message = "Hello";
+					model.BasicPublish("", "", false, true, properties, Encoding.UTF8.GetBytes(message));
+				}	
+			}
+		}
+	}
+}
